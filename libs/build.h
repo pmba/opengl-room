9bcf8795c2b41239f81b7d2a719cf77284526424
Phyllipe Bezerra <pmba@ic.ufal.br>
2020-12-17 18:47:24 -0300

    Adiciona Iluminação e Spotlight na Mesa

--- libs/build.h
+++ libs/build.h
@@ -6,6 +6,8 @@
 #include <string>
 
 #include "texture.h"
+#include "material.h"
+#include "solids.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
@@ -13,14 +15,50 @@ const GLfloat roomWidth = 20;
 const GLfloat roomHeight = 10;
 const GLfloat roomDepth = 1;
 
+const glm::vec2 windowPosition = glm::vec2({ 6, 4 });
+const glm::vec3 windowDimention = glm::vec3({ 7.5, 5, 0.2 });
+
+glm::vec3 rgb(GLfloat r, GLfloat g, GLfloat b)
+{
+    return glm::vec3(r/255, g/255, b/255);
+}
+
 GLfloat* getNormal(glm::vec3 u, glm::vec3 v)
 {
     glm::vec3 normal = glm::normalize(glm::cross(u, v));
     return glm::value_ptr(normal);
 }
 
-void displayText(int windowsWidth, int windowsHeight) 
+void drawCrosshair(int windowWidth, int windowHeight)
 {
+    glPushMatrix();
+    glViewport(0, 0, windowWidth, windowHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, windowWidth, windowHeight, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glColor3f(0, 0, 0);
+    glLineWidth(2.0);
+    glBegin(GL_LINES);
+    //horizontal line
+    glVertex2i(windowWidth / 2 - 10, windowHeight / 2);
+    glVertex2i(windowWidth / 2 + 10, windowHeight / 2);
+    glEnd();
+    //vertical line
+    glBegin(GL_LINES);
+    glVertex2i(windowWidth / 2, windowHeight / 2 + 10);
+    glVertex2i(windowWidth / 2, windowHeight / 2 - 10);
+    glEnd();
+
+    glPopMatrix();
+}
+
+void displayText(int windowsWidth, int windowsHeight, std::string s) 
+{
+    glDisable(GL_LIGHTING);
+
     glPushMatrix();
     glViewport(0, 0, windowsWidth, windowsHeight);
     glMatrixMode(GL_PROJECTION);
@@ -34,28 +72,17 @@ void displayText(int windowsWidth, int windowsHeight)
 
     glRasterPos2i(initX, initY);
 
-    std::string s = "PRESSIONE [F] PARA INTERAGIR";
     void * font = GLUT_BITMAP_HELVETICA_18;
     for (std::string::iterator i = s.begin(); i != s.end(); ++i)
     {
         char c = *i;
-        glColor3f(1.0, 1.0, 1.0);
+        glColor3f(0.0, 0.0, 0.0);
         glutBitmapCharacter(font, c);
     }
 
-    glColor3f(0.0, 0.0, 0.0);
-
-    initY -= 36;
-    initX -= 24;
-
-    glBegin(GL_QUADS);
-        glVertex2f(initX, initY);
-        glVertex2f(initX + 28*12, initY);
-        glVertex2f(initX + 28*12, initY + 54);
-        glVertex2f(initX, initY + 54);
-    glEnd();
-
     glPopMatrix();
+
+    glEnable(GL_LIGHTING);
 }
 
 void buildWall(
@@ -64,37 +91,48 @@ void buildWall(
 {
     glColor3f(1, 1, 1);
     // Front Face
-    glNormal3fv(getNormal({ width, 0, 0 }, { 0, height, 0 }));
+
+    glNormal3f(0, 0, -1);
     glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
     glTexCoord2f(texCoordH, 0.0); glVertex3f(width, 0.0, 0.0);
     glTexCoord2f(texCoordH, texCoordV); glVertex3f(width, height, 0.0);
     glTexCoord2f(0.0, texCoordV); glVertex3f(0.0, height, 0.0);
+    
     // Back Face
-    glNormal3fv(getNormal({ width, 0, depth }, { 0, height, depth }));
+
+    glNormal3f(0, 0, 1);
     glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, depth);
     glTexCoord2f(texCoordH, 0.0); glVertex3f(width, 0.0, depth);
     glTexCoord2f(texCoordH, texCoordV); glVertex3f(width, height, depth);
     glTexCoord2f(0.0, texCoordV); glVertex3f(0.0, height, depth);
+
     // Left Face
-    glNormal3fv(getNormal({ 0, height, 0 }, { 0, 0, depth }));
-    glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
-    glTexCoord2f(0.0, texCoordV); glVertex3f(0.0, height, 0.0);
-    glTexCoord2f(texCoordH, texCoordV); glVertex3f(0.0, height, depth);
+
+    glNormal3f(-1, 0, 0);
     glTexCoord2f(texCoordH, 0.0); glVertex3f(0.0, 0.0, depth);
+    glTexCoord2f(texCoordH, texCoordV); glVertex3f(0.0, height, depth);
+    glTexCoord2f(0.0, texCoordV); glVertex3f(0.0, height, 0.0);
+    glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
+
     // Right Face
-    glNormal3fv(getNormal({ 0, height, 0 }, { 0, 0, depth }));
+
+    glNormal3f(1, 0, 0);
     glTexCoord2f(0.0, 0.0); glVertex3f(width, 0.0, 0.0);
     glTexCoord2f(0.0, texCoordV); glVertex3f(width, height, 0.0);
     glTexCoord2f(texCoordH, texCoordV); glVertex3f(width, height, depth);
     glTexCoord2f(texCoordH, 0.0); glVertex3f(width, 0.0, depth);
+
     // Bottom Face
-    glNormal3fv(getNormal({ width, 0, 0 }, { 0, 0, depth }));
+
+    glNormal3f(0, -1, 0);
     glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
     glTexCoord2f(0.0, texCoordV); glVertex3f(width, 0.0, 0.0);
     glTexCoord2f(texCoordH, texCoordV); glVertex3f(width, 0.0, depth);
     glTexCoord2f(texCoordH, 0.0); glVertex3f(0.0, 0.0, depth);
+    
     // Top Face
-    glNormal3fv(getNormal({ width, 0, 0 }, { 0, 0, depth }));
+    
+    glNormal3f(0, 1, 0);
     glTexCoord2f(0.0, 0.0); glVertex3f(0.0, height, 0.0);
     glTexCoord2f(0.0, texCoordV); glVertex3f(width, height, 0.0);
     glTexCoord2f(texCoordH, texCoordV); glVertex3f(width, height, depth);
@@ -129,6 +167,75 @@ void buildDoorFrame(Texture* texture)
         glBegin(GL_QUADS);
             buildWall(5.0, 0.4, roomDepth);
         glEnd();
+    glPopMatrix();
+
+    glFlush();
+    glDisable(GL_TEXTURE_2D);
+}
+
+void buildBoard(Texture* texture, glm::vec3 color, glm::vec3 dimentions)
+{
+    GLfloat width = dimentions.x;
+    GLfloat height = dimentions.y;
+    GLfloat depth = dimentions.z;
+
+    glEnable(GL_TEXTURE_2D);
+
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glBindTexture(GL_TEXTURE_2D, texture->id);
+
+    glColor3fv(glm::value_ptr(color));
+    glPushMatrix();
+
+        glRotatef(180, 0, 1, 0);
+        glTranslatef(-roomWidth + 3.5, 6, roomWidth - depth);
+
+        glBegin(GL_QUADS);
+
+            // Front Face
+            glNormal3fv(getNormal({ width, 0, 0 }, { 0, height, 0 }));
+            glTexCoord2f(1.0, 1.0); glVertex3f(0.0, 0.0, 0.0);
+            glTexCoord2f(0.0, 1.0); glVertex3f(width, 0.0, 0.0);
+            glTexCoord2f(0.0, 0.0); glVertex3f(width, height, 0.0);
+            glTexCoord2f(1.0, 0.0); glVertex3f(0.0, height, 0.0);
+            // Back Face
+            glNormal3fv(getNormal({ width, 0, depth }, { 0, height, depth }));
+            glTexCoord2f(1.0, 1.0); glVertex3f(0.0, 0.0, depth);
+            glTexCoord2f(0.0, 1.0); glVertex3f(width, 0.0, depth);
+            glTexCoord2f(0.0, 0.0); glVertex3f(width, height, depth);
+            glTexCoord2f(1.0, 0.0); glVertex3f(0.0, height, depth);
+
+        glEnd();
+        
+        glDisable(GL_TEXTURE_2D);
+
+        glBegin(GL_QUADS);
+            // Left Face
+            glNormal3fv(getNormal({ 0, height, 0 }, { 0, 0, depth }));
+            glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
+            glTexCoord2f(0.0, 1.0); glVertex3f(0.0, height, 0.0);
+            glTexCoord2f(1.0, 1.0); glVertex3f(0.0, height, depth);
+            glTexCoord2f(1.0, 0.0); glVertex3f(0.0, 0.0, depth);
+            // Right Face
+            glNormal3fv(getNormal({ 0, height, 0 }, { 0, 0, depth }));
+            glTexCoord2f(0.0, 0.0); glVertex3f(width, 0.0, 0.0);
+            glTexCoord2f(0.0, 1.0); glVertex3f(width, height, 0.0);
+            glTexCoord2f(1.0, 1.0); glVertex3f(width, height, depth);
+            glTexCoord2f(1.0, 0.0); glVertex3f(width, 0.0, depth);
+            // Bottom Face
+            glNormal3fv(getNormal({ width, 0, 0 }, { 0, 0, depth }));
+            glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
+            glTexCoord2f(0.0, 1.0); glVertex3f(width, 0.0, 0.0);
+            glTexCoord2f(1.0, 1.0); glVertex3f(width, 0.0, depth);
+            glTexCoord2f(1.0, 0.0); glVertex3f(0.0, 0.0, depth);
+            // Top Face
+            glNormal3fv(getNormal({ width, 0, 0 }, { 0, 0, depth }));
+            glTexCoord2f(0.0, 0.0); glVertex3f(0.0, height, 0.0);
+            glTexCoord2f(0.0, 1.0); glVertex3f(width, height, 0.0);
+            glTexCoord2f(1.0, 1.0); glVertex3f(width, height, depth);
+            glTexCoord2f(1.0, 0.0); glVertex3f(0.0, height, depth);
+        glEnd();
+
     glPopMatrix();
 
     glFlush();
@@ -213,9 +320,34 @@ void buildFrontWall(Texture* texture)
     glPushMatrix();
         glTranslatef(0.0, 0.0, -20.0 - roomDepth);
 
+        // Bottom part
         glBegin(GL_QUADS);
-            buildWall(roomWidth, roomHeight, roomDepth);
+            buildWall(roomWidth, windowPosition.y, roomDepth);
         glEnd();
+
+        // Top Part
+        glPushMatrix();
+            glTranslatef(0.0, windowPosition.y + windowDimention.y, 0.0);
+            glBegin(GL_QUADS);
+                buildWall(roomWidth, roomHeight - (windowPosition.y + windowDimention.y), roomDepth);
+            glEnd();
+        glPopMatrix();
+
+         // Left part
+        glPushMatrix();
+            glTranslatef(0.0, windowPosition.y, 0.0);
+            glBegin(GL_QUADS);
+                buildWall(windowPosition.x, windowDimention.y, roomDepth);
+            glEnd();
+        glPopMatrix();
+        
+        // Right part
+        glPushMatrix();
+            glTranslatef(windowPosition.x + windowDimention.x, windowPosition.y, 0.0);
+            glBegin(GL_QUADS);
+                buildWall(roomWidth - (windowPosition.x + windowDimention.x), windowDimention.y, roomDepth);
+            glEnd();
+        glPopMatrix();
     glPopMatrix();
 
     glFlush();
@@ -330,85 +462,138 @@ void buildRoof(Texture* texture)
     glDisable(GL_TEXTURE_2D);
 }
 
-void buildWindow(Texture* texture)
+void buildTable(bool alignLeft)
 {
-    glEnable(GL_TEXTURE_2D);
+    GLfloat width = 7.f;
+    GLfloat height = roomHeight / 3;
+    GLfloat depth = 3.f;
+    GLfloat thickness = 0.2f;
 
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-    glBindTexture(GL_TEXTURE_2D, texture->id);
-    
+    GLfloat offset = alignLeft ? roomWidth - thickness : 0;
+
+    glColor3f(1, 1, 1);
     glPushMatrix();
+        glRotatef(-90.f, 0, 1, 0);
+        glTranslatef(-roomWidth, 0, -roomWidth + (alignLeft ? roomWidth - width : 0));
 
-        glTranslatef(0.0, 0.0, -0.3);
-
-        glNormal3fv(getNormal({ 8.0, 0.0, 0.0 }, { 0.0, 5.0, 0.0 }));
         glBegin(GL_QUADS);
-
-        glTexCoord2f(0.0, 1.0); glVertex3f(6.0, 3.0, -19.5);
-        glTexCoord2f(1.0, 1.0); glVertex3f(14.0, 3.0, -19.5);
-        glTexCoord2f(1.0, 0.0); glVertex3f(14.0, 8.0, -19.5);
-        glTexCoord2f(0.0, 0.0); glVertex3f(6.0, 8.0, -19.5);
-
+            buildWall(depth, height - thickness, thickness);
         glEnd();
 
-        glFlush();
-        glDisable(GL_TEXTURE_2D);
+    glPopMatrix();
 
-        // Left Border ...........................................................................
+    glPushMatrix();
+        glRotatef(-90.f, 0, 1, 0);
+        glTranslatef(-roomWidth, 0, -thickness + (alignLeft ? 0 : width - roomWidth));
 
-        glNormal3fv(getNormal({ 0.0, 5.0, 0.0 }, { 0.0, 0.0, 0.5 }));
         glBegin(GL_QUADS);
-        glColor3f(0.647, 0.651, 0.631);
-
-        glTexCoord2f(0.0, 1.0); glVertex3f(6.0, 3.0, -20.0);
-        glTexCoord2f(1.0, 1.0); glVertex3f(6.0, 8.0, -20.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f(6.0, 8.0, -19.5);
-        glTexCoord2f(0.0, 0.0); glVertex3f(6.0, 3.0, -19.5);
-
+            buildWall(depth, height - thickness, thickness);
         glEnd();
-        glFlush();
 
-        // Right Border ..........................................................................
+    glPopMatrix();
 
-        glNormal3fv(getNormal({ 0.0, 5.0, 0.0 }, { 0.0, 0.0, 0.5 }));
+    glPushMatrix();
+        glRotatef(-90.f, 1, 0, 0);
+        glTranslatef((alignLeft ? 0 : roomWidth - width), roomWidth - depth, height - thickness);
+
         glBegin(GL_QUADS);
-        glColor3f(0.647, 0.651, 0.631);
-
-        glTexCoord2f(0.0, 1.0); glVertex3f(14.0, 3.0, -20.0);
-        glTexCoord2f(1.0, 1.0); glVertex3f(14.0, 8.0, -20.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f(14.0, 8.0, -19.5);
-        glTexCoord2f(0.0, 0.0); glVertex3f(14.0, 3.0, -19.5);
-
+            buildWall(7, depth, thickness);
         glEnd();
-        glFlush();
 
-        // Top Border ............................................................................
+    glPopMatrix();
+    glFlush();
+}
 
-        glNormal3fv(getNormal({ 0.0, 0.0, 0.5 }, { 8.0, 0.0, 0.0 }));
-        glBegin(GL_QUADS);
-        glColor3f(0.647, 0.651, 0.631);
+void buildWindowGlass() 
+{
+    glPushMatrix();
+        glm::vec3 innerWindowDimentions = glm::vec3({ 
+            (windowDimention.x - (2 * windowDimention.z)) / 2,
+            windowDimention.y - (2 * windowDimention.z),
+            0.3
+        });
 
-        glTexCoord2f(0.0, 1.0); glVertex3f(6.0, 8.0, -19.5);
-        glTexCoord2f(1.0, 1.0); glVertex3f(6.0, 8.0, -20.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f(14.0, 8.0, -20.0);
-        glTexCoord2f(0.0, 0.0); glVertex3f(14.0, 8.0, -19.5);
+        glColor3fv(glm::value_ptr(rgb(130, 130, 130)));
+        drawSolidCuboid(0.2, innerWindowDimentions.y, innerWindowDimentions.z);
 
-        glEnd();
-        glFlush();
+        glPushMatrix();
+            glTranslatef(innerWindowDimentions.x, 0, 0);
+            drawSolidCuboid(0.2, innerWindowDimentions.y, innerWindowDimentions.z);
+        glPopMatrix();
 
-        // Bottom Border .........................................................................
+        glPushMatrix();
+            glTranslatef(0.2, 0, 0);
+            drawSolidCuboid(innerWindowDimentions.x - 0.2, 0.2, innerWindowDimentions.z);
+        glPopMatrix();
+        
+        glPushMatrix();
+            glTranslatef(0.2, innerWindowDimentions.y - 0.2, 0);
+            drawSolidCuboid(innerWindowDimentions.x - 0.2, 0.2, innerWindowDimentions.z);
+        glPopMatrix();
 
-        glNormal3fv(getNormal({ 0.0, 0.0, 0.5 }, { 8.0, 0.0, 0.0 }));
-        glBegin(GL_QUADS);
-        glColor3f(0.647, 0.651, 0.631);
+        glBlendFunc(GL_ZERO, GL_SRC_COLOR);
+        glEnable( GL_BLEND );
+        glPushMatrix();
 
-        glTexCoord2f(0.0, 1.0); glVertex3f(6.0, 3.0, -19.5);
-        glTexCoord2f(1.0, 1.0); glVertex3f(6.0, 3.0, -20.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f(14.0, 3.0, -20.0);
-        glTexCoord2f(0.0, 0.0); glVertex3f(14.0, 3.0, -19.5);
+            glColor3f(0.8, 0.8, 0.8);
+            glTranslatef(0.2, 0, 0);
+            glBegin(GL_QUADS);
+                glVertex3f(0, 0, 0);
+                glVertex3f(innerWindowDimentions.x - 0.2, 0, 0.1);
+                glVertex3f(innerWindowDimentions.x - 0.2, innerWindowDimentions.y, 0.1);
+                glVertex3f(0, innerWindowDimentions.y, 0.1);
+            glEnd();
 
-        glEnd();
-        glFlush();
+        glPopMatrix();
+        glDisable( GL_BLEND );
 
+    glPopMatrix();
+}
+
+void buildWindow(Material chrome)
+{
+    glPushMatrix();
+        chrome.active();
+        glColor3fv(glm::value_ptr(rgb(110, 110, 110)));
+
+        glTranslatef(windowPosition.x, windowPosition.y, -(roomWidth + roomDepth + 0.1));
+
+        drawSolidCuboid(windowDimention.z, windowDimention.y, roomDepth + 0.2);
+
+        glPushMatrix();
+            glTranslatef(windowDimention.x - windowDimention.z, 0, 0);
+            drawSolidCuboid(windowDimention.z, windowDimention.y, roomDepth + 0.2);
+        glPopMatrix();
+
+        glPushMatrix();
+            drawSolidCuboid(windowDimention.x, windowDimention.z, roomDepth + 0.2);
+        glPopMatrix();
+
+        glPushMatrix();
+            glTranslatef(0, windowDimention.y - windowDimention.z, 0);
+            drawSolidCuboid(windowDimention.x, windowDimention.z, roomDepth + 0.2);
+        glPopMatrix();
+    glPopMatrix();
+}
+
+void buildShelfWithBooks()
+{
+    glPushMatrix();
+        glTranslatef(0, 3*roomHeight/4, -roomWidth);
+        // Shelf
+        drawSolidCuboid(windowPosition.x - 0.5, 0.2, 1.5);
+        // Books
+        glMaterialf(GL_FRONT, GL_SHININESS, 100);
+        glTranslatef(0, 0.2, 0);
+        glColor3fv(glm::value_ptr(rgb(150, 0, 0)));
+        drawSolidCuboid(0.3, 1.5, 1.2);
+
+        glTranslatef(0.3, 0, 0);
+        glColor3fv(glm::value_ptr(rgb(10, 100, 150)));
+        drawSolidCuboid(0.3, 1.3, 1.0);
+
+        glTranslatef(0.3, 0, 0);
+        glColor3fv(glm::value_ptr(rgb(150, 100, 0)));
+        drawSolidCuboid(0.3, 1.2, 0.9);
     glPopMatrix();
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-#define GL_GLEXT_PROTOTYPES
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glut.h>
+// #define GL_GLEXT_PROTOTYPES
+#define GLEW_STATIC
+// #include <GL/gl.h>
+// #include <GL/glu.h>
+// #include <GL/glut.h>
+#include <GL/glew.h>
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "./libs/stb_image.h"
@@ -11,6 +14,10 @@
 #include "./libs/build.h"
 #include "./libs/texture.h"
 #include "./libs/animation.h"
+#include "./libs/solids.h"
+#include "./libs/material.h"
+
+#include "./libs/obj_loader.h"
 
 static int windows_w = 1080, windows_h = 720;
 
@@ -21,13 +28,51 @@ Texture wallTexture;
 Texture windowTexture;
 Texture doorTexture;
 Texture doorFrameTexture;
+Texture homeOfficeTexture;
+Texture booksTexture;
+
+// Materials
+
+Material ambient(0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 1.0, 1.0, 1.0, 50.0);
+Material chrome(0.25, 0.25, 0.25, 0.4, 0.4, 0.4, 0.774597, 0.774597, 0.774597, 0.6);
+Material silver(0.19225, 0.19225, 0.19225, 0.50754, 0.50754, 0.50754, 0.508273, 0.508273, 0.508273, 0.4);
+
+// Light Props
+
+static GLfloat light0_ambient[]	    = { 0.2, 0.2, 0.2, 1.0 };
+static GLfloat light0_diffuse[]	    = { 0.8, 0.8, 0.8, 1.0 };
+static GLfloat light0_specular[]	= { 1.0, 1.0, 1.0, 1.0 };
+static GLfloat light0_position[]	= { 0.0, 0.0, 0.0, 1.0 };
+
+static bool light1_enabled          = true;
+static GLfloat light1_offset[]	    = { 19.4, 4, -19.2 };
+static GLfloat light1_ambient[]	    = { 0.2, 0.2, 0.2, 1.0 };
+static GLfloat light1_diffuse[]	    = { 1.0, 1.0, 1.0, 1.0 };
+static GLfloat light1_specular[]	= { 1.0, 1.0, 1.0, 1.0 };
+static GLfloat light1_position[]	= { 0.0, 3.0, 0.0, 1.0 };
+static GLfloat light1_direction[]	= { 0.0, -1.0, 0.0 };
+static GLfloat light1_angle	        = 20.0;
+static GLfloat light1_exponent	    = 2.0;
+
+// Quadric
+
+GLUquadric* cylinderQuadric;
+
+// Loader
+
+objl::Loader loader;
 
 // Animations
 
 AnimationState doorAnimation = IDLE;
 GLfloat doorAngle = 0.f;
 
+AnimationState windowAnimation = IDLE;
+GLfloat windowTranslation = 1.f;
+
 using namespace std;
+
+void onPositionUpdated(glm::vec3 position);
 
 void loadTexture(const char* fileName, Texture* texture) 
 {
@@ -70,35 +115,10 @@ void setupTexture(Texture* texture)
                     texture->data);                  
 }
 
-void init(void)
-{    
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
-    glShadeModel(GL_FLAT);
-    glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+void initTextures()
+{
+    // Loading Textures ......................................................................
 
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_COLOR_MATERIAL);
-
-    // Lighting setup
-	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-
-	// Set lighting intensity and color
-	GLfloat qaAmbientLight[]	= {0.2, 0.2, 0.2, 1.0};
-	GLfloat qaDiffuseLight[]	= {0.8, 0.8, 0.8, 1.0};
-	GLfloat qaSpecularLight[]	= {1.0, 1.0, 1.0, 1.0};
-	glLightfv(GL_LIGHT0, GL_AMBIENT, qaAmbientLight);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, qaDiffuseLight);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, qaSpecularLight);
-
-	// Set the light position
-	// GLfloat qaLightPosition[]	= {10.0, 10.0, -10.0, 1.0};
-	GLfloat qaLightPosition[]	= {0.0, 0.0, 0.0, 1.0};
-	glLightfv(GL_LIGHT0, GL_POSITION, qaLightPosition);
-    
     loadTexture("./res/textures/wall.png", &wallTexture);
     setupTexture(&wallTexture); 
 
@@ -113,6 +133,88 @@ void init(void)
 
     loadTexture("./res/textures/door-frame.png", &doorFrameTexture);
     setupTexture(&doorFrameTexture);
+
+    loadTexture("./res/textures/home-office.png", &homeOfficeTexture);
+    setupTexture(&homeOfficeTexture);
+
+    loadTexture("./objs/books/books.png", &booksTexture);
+    setupTexture(&booksTexture);
+}
+
+void init(void)
+{   
+    glClearColor (0.31, 0.61, 0.85, 1.0);
+
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+
+    // Anti aliasing smoothing props ........................................................
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_POINT_SMOOTH);
+    glEnable(GL_BLEND);
+
+    // Lighting and Shade setup .............................................................
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL); 
+    glShadeModel(GL_SMOOTH);
+ 
+    // Turn on OpenGL lighting ..............................................................
+    glEnable(GL_LIGHTING); 
+    
+	// Light 0 property vectors .............................................................
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+
+    glEnable(GL_LIGHT0);
+
+    // End Light 0 ..........................................................................
+    // Light 1 property vectors .............................................................
+
+    glLightfv(GL_LIGHT1, GL_AMBIENT, light1_ambient);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, light1_specular);
+    glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+    
+    glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, light1_angle);
+    glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, light1_direction);
+    glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, light1_exponent);
+
+    glEnable(GL_LIGHT1);
+
+    // End Light 1 ..........................................................................
+
+   // Enable color material mode:
+   // The ambient and diffuse color of the front faces will track the color set by glColor().
+   glEnable(GL_COLOR_MATERIAL); 
+   glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE); 
+
+   initTextures();
+}
+
+void setupLightning()
+{
+    glEnable(GL_LIGHTING); 
+
+    glEnable(GL_LIGHT0);
+    glPushMatrix();
+        glTranslatef(roomWidth / 2, roomHeight - 0.5, -(roomWidth / 2));
+        GLfloat light0_position[]	= {0, 0, 0, 1};
+        glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+    glPopMatrix();
+
+    if (light1_enabled) {
+        glEnable(GL_LIGHT1);
+        glPushMatrix();
+            glTranslatef(light1_offset[0], light1_offset[1], light1_offset[2]);
+
+            glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+            glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, light1_angle);
+            glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, light1_direction);
+            glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, light1_exponent);
+        glPopMatrix();
+    }
 }
 
 void setupCamera() 
@@ -140,16 +242,44 @@ void checkDoorAngle()
     }
 }
 
+void checkWindowTranslation()
+{
+    if (windowAnimation == FORWARDS || windowAnimation == BACKWARDS)
+    {
+        windowTranslation += windowAnimation == FORWARDS ? 0.02 : -0.02;
+        if (windowTranslation >= 1.0f || windowTranslation <= 0.0f) windowAnimation = IDLE;
+    }
+}
+
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor (0.31, 0.61, 0.85, 1.0);
 
-    displayText(windows_w, windows_h);
+    // displayText(windows_w, windows_h, actionText);
+    drawCrosshair(windows_w, windows_h);
 
     setupCamera();
+    setupLightning();
+
+    glPushMatrix();
+      glTranslatef(light1_offset[0], light1_offset[1], light1_offset[2]); // Move the spotlight.
+      
+      // Draw the spotlight cone in wireframe after disabling lighting
+      glPushMatrix();
+         glDisable(GL_LIGHTING);
+            glRotatef(-90.0, 1.0, 0.0, 0.0);
+            glColor3f(1.0, 0.0, 1.0);
+            glutSolidCone(3.0 * tan( light1_angle/180.0 * M_PI ), 3.0, 20, 20);
+         glEnable(GL_LIGHTING);
+      glPopMatrix();
+    
+   glPopMatrix();
+
+    ambient.active();
 
     buildFrontWall(&wallTexture);
-    buildBackWall(&wallTexture);
+    // buildBackWall(&wallTexture);
     buildLeftWall(&wallTexture);
     buildRightWall(&wallTexture);
     buildRoof(&wallTexture);
@@ -159,8 +289,26 @@ void display(void)
 
     checkDoorAngle();
     buildDoor(&doorTexture, doorAngle);
-    
-    buildWindow(&windowTexture);
+
+    buildBoard(&homeOfficeTexture, rgb(154, 149, 143), {1.5, 1.8618, 0.1});
+    buildTable(false);
+    // buildTable(true);
+
+    checkWindowTranslation();
+    glPushMatrix();
+        glTranslatef(windowPosition.x + 0.2, windowPosition.y + 0.2, -(roomWidth + 0.4));
+        buildWindowGlass();
+    glPopMatrix();
+
+    glPushMatrix();
+        GLfloat windowTranslationOffset = ((windowDimention.x - (2 * windowDimention.z)) / 2) * windowTranslation;
+        glTranslatef((windowPosition.x) + windowTranslationOffset, windowPosition.y + 0.2, -(roomWidth + 0.6));
+        buildWindowGlass();
+    glPopMatrix();
+
+    buildShelfWithBooks();
+
+    buildWindow(silver);
 
     glutSwapBuffers();
 }
@@ -181,7 +329,7 @@ void reshape(int w, int h)
 
 void keyboard (unsigned char key, int x, int y)
 {
-   switch (key) {
+    switch (key) {
         case 27:
             exit(0);
             break;
@@ -203,12 +351,58 @@ void keyboard (unsigned char key, int x, int y)
         case 'e':
             camera.moveDown();
             break;
-        case 'f':
+        case 'p':
             doorAnimation = doorAngle == 90.0f ? BACKWARDS : FORWARDS;
+            break;
+        case 'j':
+            windowAnimation = windowTranslation == 1.0f ? BACKWARDS : FORWARDS;
+            break;
+        case 'l':
+            light1_enabled = !light1_enabled;
+            if (!light1_enabled) glDisable(GL_LIGHT1);
+            break;
+
+        case '4':
+            light1_offset[0] -= 0.2;
+            cout << light1_offset[0] << ", " << light1_offset[1] << ", " << light1_offset[2] << endl;
+            break;
+        case '5':
+            light1_offset[2] += 0.2;
+            cout << light1_offset[0] << ", " << light1_offset[1] << ", " << light1_offset[2] << endl;
+            break;
+        case '6':
+            light1_offset[0] += 0.2;
+            cout << light1_offset[0] << ", " << light1_offset[1] << ", " << light1_offset[2] << endl;
+            break;
+        case '8':
+            light1_offset[2] -= 0.2;
+            cout << light1_offset[0] << ", " << light1_offset[1] << ", " << light1_offset[2] << endl;
+            break;
+        case '7':
+            light1_offset[1] += 0.2;
+            cout << light1_offset[0] << ", " << light1_offset[1] << ", " << light1_offset[2] << endl;
+            break;
+        case '9':
+            light1_offset[1] -= 0.2;
+            cout << light1_offset[0] << ", " << light1_offset[1] << ", " << light1_offset[2] << endl;
+            break;
+        
+        default:
+            break;
+    }
+}
+
+void specialKeyInput(int key, int x, int y)
+{
+    switch (key) {
+        case GLUT_KEY_F1:
+            glDisable(GL_LIGHT0);
             break;
         default:
             break;
-   }
+    }
+
+    glutPostRedisplay();
 }
 
 void motion(int x, int y)
@@ -227,15 +421,21 @@ int main(int argc, char** argv)
    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
    glutInitWindowSize(windows_w, windows_h);
    glutInitWindowPosition(100, 100);
-   glutCreateWindow(argv[0]);
-   init();
+   
+   glutCreateWindow("Trabalho Final - CG");
 
    glutDisplayFunc(display);
    glutReshapeFunc(reshape);
    glutKeyboardFunc(keyboard);
+   glutSpecialFunc(specialKeyInput);
    glutMotionFunc(motion);
    glutTimerFunc(30, update, 0);
 
+    // bool loadout = loader.LoadFile("./objs/books/books.obj");
+
+    // cout << "Loadout: " << loadout << endl;
+
+   init();
    glutMainLoop();
    return 0; 
 }
